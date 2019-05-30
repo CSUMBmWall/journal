7c1114051a63c3d4eb0585ae7ae827d6febfbc25
CSUMBmWall <mwall@csumb.edu>
2019-05-30 15:02:51 -0700

    Update imageService.ts

--- journal_server/src/api/routes/imageService.ts
+++ journal_server/src/api/routes/imageService.ts
@@ -30,28 +30,9 @@ router.get('/', ( req: any, res: Response, next: any) => {
             for (const pic of hit._source.img) {
                 images.push(pic);
             }
-
-            // console.log('hit ' + JSON.stringify(hit));
         }
         res.send(images);
-        //res.send(searchResponse.hits.hits);
     });
-
-    // console.log('hits' + JSON.parse(response));
-
-    //res.status(200).send(response.hits.hits)
-    // res.status(200).json({hits: response.hits.hits})
-
-    /*try {
-        res.status(200).json({hits: response.hits.hits})
-        // res.send(response.hits.hits);
-    } catch(er) {
-        res.status(404).json({error: er});
-    }*/
-
-
-
-
 });
 
 module.exports = router;
