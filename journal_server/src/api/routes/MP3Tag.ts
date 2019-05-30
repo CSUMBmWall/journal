29a3cfd2aad05949b83ace6095989f12c5a6660f
CSUMBmWall <mwall@csumb.edu>
2019-05-30 15:01:11 -0700

    Update MP3Tag.ts

--- journal_server/src/api/routes/MP3Tag.ts
+++ journal_server/src/api/routes/MP3Tag.ts
@@ -10,7 +10,6 @@ const NodeID3 = require('node-id3');
 const fs = require('fs');
 const path = require('path');
 
-/*file = "D:\\Music\\Jerry Lee Lewis\\Jerry Lee Lewis - Anthology (All Killer No Filler) Disc 1 - Crazy Arms.mp3"*/
 let file = "D:\\Music\\Aretha Franklin - YouTube -  Do Right Woman Do Right Man.mp3";
 
 // Get file info
@@ -28,18 +27,6 @@ router.post('/write', (req: Request, res: Response) => {
     let img = req.body.tag.APIC;
     let tempFile = 'temp';
     if (img) { tempFile += path.extname(img);}
-
-    // downloadImage(img, tempFile)
-    //     .then(NodeID3.update(req.body.tag, req.body.fileLoc));
-
-
-
-
-
-
-
-   /*console.log(req.body);
-   res.send(req.body);*/
 });
 
 const setMP3Tag = (tag: MP3TagModel, fileLoc: string) => {
@@ -54,108 +41,11 @@ const downloadImage = function(uri: any, filename: any){
             console.log(err);
             return { error: err };
         } else {
-            // console.log('content-type:', res.headers['content-type']);
-            // console.log('content-length:', res.headers['content-length']);
-
             request(uri).pipe(fs.createWriteStream(filename))
                 .then(() => {return});
         }
     });
 
 };
-
-/*
-const downloadImage = function(uri: any, filename: any, callback: any){
-    request.head(uri, function(err: Error, res: reqRes, body: any){
-        if (err) {
-            console.log(err);
-        } else {
-            // console.log('content-type:', res.headers['content-type']);
-            // console.log('content-length:', res.headers['content-length']);
-
-            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-        }
-    });
-
-};
-
-*/
-
-/*download('http://afropea-mag.com/wp-content/uploads/2016/06/aretha-franklin-770.jpg', 'google.png', function(){
-    console.log('done');
-});*/
-
-// async function
-
-
-
-
-/*async function foo() {
-    try {
-        const result = await doSomething();
-        const newResult = await doSomethingElse(result);
-        const finalResult = await doThirdThing(newResult);
-        console.log(`Got the final result: ${finalResult}`);
-    } catch(error) {
-        failureCallback(error);
-    }
-}*/
-
-/*
-* download image file if it is in req.body
-* then write MP3 tag
-* then delete image file
-* then return success!
-* */
-
-
-
-
-
-
-
-
-/*exports.getID3Tags = function(req: Request, res: Response) {
-    const fileLoc = req.body.fileLoc;
-    let tags: MP3TagModel = NodeID3.read(fileLoc)
-    NodeID3.read(fileLoc, function(err: Error, tags: MP3TagModel) {
-        /!*
-        tags: {
-          title: "Tomorrow",
-          artist: "Kevin Penkin",
-          image: {
-            mime: "jpeg",
-            type: {
-              id: 3,
-              name: "front cover"
-            },
-            description: String,
-            imageBuffer: Buffer
-          },
-          raw: {
-            TIT2: "Tomorrow",
-            TPE1: "Kevin Penkin",
-            APIC: Object (See above)
-          }
-        }
-        *!/
-        console.log(tags);
-        res.send({tags: tags});
-    })
-
-};*/
-
-/*exports.setID3Tags = function() {
-    return null;
-};*/
-
-
-
-/*exports.removeTag = function(req, res, filepath){
-    let success = NodeID3.removeTags(filepath)  //  returns true/false
-    NodeID3.removeTags(filepath, function(err) {
-        console.log(error);
-    })
-}*/
 
 module.exports = router;
