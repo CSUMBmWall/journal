c7cc7486f916d83c4da825624723f68231920cfa
CSUMBmWall <mwall@csumb.edu>
2019-05-30 15:05:04 -0700

    Update youTubeDownload.ts

--- journal_server/src/api/routes/youTubeDownload.ts
+++ journal_server/src/api/routes/youTubeDownload.ts
@@ -4,38 +4,6 @@ const express = require('express');
 const router = express.Router();
 var youTubeDLService = require('youtube-dl');
 
-/*var fs = require('fs');
-var ytdl = require('ytdl-core');
-
-const { spawn } = require('child_process');*/
-
-/*exports.getYouTubeDL = function(req, res) {
-    /!*console.log('youtubeDL.js.videoInfo.url', req.query.url);*!/
-    const url =  req.query.url;
-    const folder = req.query.folder;
-    const fileName = req.query.artist + ' - ' + req.query.album + ' - ' + req.query.title;
-    const outPutFileName = folder + fileName;
-
-    youTubeDLService.exec(url, ['-x', '--audio-format', 'mp3', '-o', folder + fileName + '.%(ext)s'], {}, function exec(err, output) {
-        'use strict';
-        if (err) { throw err; }
-        /!*var msgs = [];
-        output.forEach(function(msg) {
-            if (msg.includes('[ffmpeg] Destination:')) {
-                const pos = msg.indexOf(':') ;
-                const status = msg.slice(pos + 1);
-                /!*msgs.push(msg.slice(pos + 2));*!/
-                msgs.push(status);
-
-
-            }
-        })*!/
-        //console.log(typeof(output));
-        res.send({resp: output});
-        // res.send({status: output});
-    });
-};*/
-
 
 router.post('/', (req: Request, res: Response, next: any) => {
 
@@ -44,8 +12,6 @@ router.post('/', (req: Request, res: Response, next: any) => {
     const url = ytVideoInfo.url;
     const fileName = ytVideoInfo.artist + ' - ' + ytVideoInfo.album + ' - ' + ytVideoInfo.title;
     const outPutFileName = folder + fileName;
-    //
-    // console.log('url ' + url);
 
     youTubeDLService.exec(url, ['-x', '--audio-format', 'mp3', '-o', outPutFileName + '.%(ext)s'], {}, function exec(err: any, output: any) {
         'use strict';
@@ -61,38 +27,3 @@ router.post('/', (req: Request, res: Response, next: any) => {
 });
 
 module.exports = router;
-
-/*exports.getYouTubeDL = function(req: Request, res: Response) {
-    /!*console.log('youtubeDL.js.videoInfo.url', req.query.url);*!/
-    const url =  req.query.url;
-    const folder = req.query.folder;
-    const fileName = req.query.artist + ' - ' + req.query.album + ' - ' + req.query.title;
-    const outPutFileName = folder + fileName;
-
-    youTubeDLService.exec(url, ['-x', '--audio-format', 'mp3', '-o', outPutFileName + '.%(ext)s'], {}, function exec(err: any, output: any) {
-        'use strict';
-        if (err) { throw err; }
-        var re = '[ffmpeg] Destination:';
-        output = output.filter((x: any) => { return x.includes(re) });
-        if (output[0]) {
-            res.send({fileLoc: output[0].replace(re, '').trim()});
-        }
-        else { res.send({ error: "Location not Found"}); }
-
-
-        /!* var msgs = [];
-         output.forEach(function(msg) {
-             var re = /\[ffmpeg] Destination:/;
-             if (msg.includes(re)) {
-                 const pos = msg.indexOf(':') ;
-                 const status = msg.slice(pos + 1);
-                 /!*msgs.push(msg.slice(pos + 2));*!/
-                 msgs.push(status);
-
-
-             }
-         })*!/
-        // res.send({resp: output});
-        // res.send({status: output});
-    });
-};*/
