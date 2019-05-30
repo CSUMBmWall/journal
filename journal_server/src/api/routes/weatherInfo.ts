655a79b4dcf5da416af01ac854b6a5241a652198
CSUMBmWall <mwall@csumb.edu>
2019-05-30 15:03:59 -0700

    Update weatherInfo.ts

--- journal_server/src/api/routes/weatherInfo.ts
+++ journal_server/src/api/routes/weatherInfo.ts
@@ -1,5 +1,4 @@
 import {Request, Response} from "express";
-// import {Response} from "request";*/
 
 const express = require('express');
 const request = require('request');
@@ -9,9 +8,6 @@ const router = express.Router();
 const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Monterey,%20us&units=imperial&appid=60ce7f6d78dfb58ad8bb6f90b7fda9b3"
 
 router.get('/', (req: Request, res: Response) => {
-
-    // request()
-    // console.log(req.body.url);
         request(weatherUrl, function (error: Error, response: Response, body: any) {
             if (!error && response.statusCode == 200) {
                 res.status(200).json(body);
