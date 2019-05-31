e1ccf9a1a57df2a0749bb44542d8b8c935c21839
CSUMBmWall <mwall@csumb.edu>
2019-05-31 10:32:54 -0700

    removed commented code

--- journal_main/src/app/services/image.service.ts
+++ journal_main/src/app/services/image.service.ts
@@ -4,14 +4,11 @@ import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class ImageService {
-  // images: string[] = [];
   images = new EventEmitter<any>();
   constructor(private http: HttpClient) { }
 
   public getImages(): Observable<any> {
     var  URL = "http://localhost:3000/imageSearch/";
-    // console.log(this.http
-    //   .get<any>(URL));
     return this.http.get<any>(URL);
   }
 
